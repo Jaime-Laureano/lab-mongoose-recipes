da4df98fee2c62919ab5e960eb0dd2c093bdbbb2
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-02-11 14:41:35 +0000

    Completed lab for students

--- Index-async-await.js
+++ Index-async-await.js
@@ -1,4 +1,4 @@
-// Solution without promiseAll
+//Async await solution
 
 const mongoose = require("mongoose");
 
@@ -22,42 +22,43 @@ let newRecipe = {
 };
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then((x) => {
+
+async function dataBaseManage() {
+  try {
+    let x = await mongoose.connect(MONGODB_URI);
     console.log(`Connected to the database: "${x.connection.name}"`);
+
     // Before adding any documents to the database, let's delete all previous entries
-    return Recipe.deleteMany();
-  })
-  .then(() => {
+    await Recipe.deleteMany();
+
     // iteration 2. newRecipe will be created in our DB
-    return Recipe.create(newRecipe); // returning the promise allows us to resolve it in the next .then(), instead of nested promises.
-  })
-  .then((result) => {
-    console.log(`recipe added: ${result.title}`); // a console.log to verify previous promise resolved correctly
+    let createdRecipe = await Recipe.create(newRecipe);
+    console.log(`recipe added: ${createdRecipe.title}`); // a console.log to verify previous promise resolved correctly
+
     // iteration 3. all recipes in data will be inserted in our DB
-    return Recipe.insertMany(data);
-  })
-  .then((result) => {
-    result.forEach((item) =>
-      console.log(`recipe for ${item.title} inserted successfully`)
+    let insertedRecipes = await Recipe.insertMany(data);
+    insertedRecipes.forEach((eachRecipe) =>
+      console.log(`recipe for ${eachRecipe.title} inserted successfully`)
     ); // a console.log to verify previous promise resolved correctly
+
     // iteration 4. To find a recipe by its title and update one attribute
-    return Recipe.findOneAndUpdate(
+    let updatedRecipe = await Recipe.findOneAndUpdate(
       { title: "Rigatoni alla Genovese" },
       { duration: 100 },
       { new: true }
     );
-  })
-  .then((result) => {
-    console.log(`The recipe ${result.title} is updated`); // a console.log to verify previous promise resolved correctly
+    console.log(`The recipe ${updatedRecipe.title} is updated`); // a console.log to verify previous promise resolved correctly
+
     // iteration 5. To find a recipe by its title and delete it
-    return Recipe.findOneAndDelete({ title: "Carrot Cake" });
-  })
-  .then((result) => {
-    console.log(`The recipe ${result.title} is deleted`); // a console.log to verify previous promise resolved correctly
+    let deletedRecipe = await Recipe.findOneAndDelete({ title: "Carrot Cake" });
+    console.log(`The recipe ${deletedRecipe.title} is deleted`); // a console.log to verify previous promise resolved correctly
+
     // iteration 6. To close the DB.
-    return mongoose.connection.close();
-  })
-  .then(() => console.log(`connection closed`)) // a console.log to verify previous promise resolved correctly
-  .catch((err) => console.log(`an error has occurred: ${err}`)); // to catch errors in promises
+    await mongoose.connection.close();
+    console.log(`connection closed`); // a console.log to verify previous promise resolved correctly
+  } catch (err) {
+    console.log(`an error has occurred: ${err}`); // to catch errors in promises
+  }
+}
+
+dataBaseManage();
